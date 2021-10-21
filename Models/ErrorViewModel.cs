fef0cb2a3e58c04649a3bdb6ec62905c2ed15198
ssrist <ssrist@micron.com>
2021-10-21 09:58:23 +0530

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCDemoAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
